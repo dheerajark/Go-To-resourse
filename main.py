277018706d877f17208bd6e980d058a8a24beac0
Dheeraj Kumar <97981620+dheerajzxcv@users.noreply.github.com>
2024-01-24 21:41:02 +0530

    add searching algorithm

--- main.py
+++ main.py
@@ -1,67 +1,77 @@
-import math
+# import math
 
 
-# 1.Implement a function to reverse a linked list
+# # 1.Implement a function to reverse a linked list
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
         
         
-class Linked_List:
-    def __init__(self):
-        self.head = None
+# class Linked_List:
+#     def __init__(self):
+#         self.head = None
         
-    def last_insert(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
+#     def last_insert(self, data):
+#         new_node = Node(data)
+#         if self.head is None:
+#             self.head = new_node
+#             return
+#         last_node = self.head
+#         while last_node.next:
+#             last_node = last_node.next
+#         last_node.next = new_node
         
-    def print_list(self):
-        # if self.head is None:
-        #     print("List is empty")
-        #     return
-        current = self.head
-        while current:
-            print(current.data, end=" => ")
-            current = current.next
-        print(None)   
+#     def print_list(self):
+#         # if self.head is None:
+#         #     print("List is empty")
+#         #     return
+#         current = self.head
+#         while current:
+#             print(current.data, end=" => ")
+#             current = current.next
+#         print(None)   
         
-    def reverse(self):
-        prev_node = None
-        current_node = self.head
-        while current_node:
-            next_node = current_node.next
-            current_node.next = prev_node
-            prev_node = current_node
-            current_node = next_node
-        prev_node = self.head
+#     def reverse(self):
+#         prev_node = None
+#         current_node = self.head
+#         while current_node:
+#             next_node = current_node.next
+#             current_node.next = prev_node
+#             prev_node = current_node
+#             current_node = next_node
+#         prev_node = self.head
         
-    def find_middle(self):
-        slow = self.head
-        fast = self.head
-        while fast.next and fast.next.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow.data   
+#     def find_middle(self):
+#         slow = self.head
+#         fast = self.head
+#         while fast.next and fast.next.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         return slow.data   
     
             
-list = Linked_List()
-list.last_insert(1)
-list.last_insert(2)
-list.last_insert(3)
-list.last_insert(4)
-list.last_insert(5)
-list.last_insert(6)
+# list = Linked_List()
+# list.last_insert(1)
+# list.last_insert(2)
+# list.last_insert(3)
+# list.last_insert(4)
+# list.last_insert(5)
+# list.last_insert(6)
 
-result = list.find_middle()
-print(result)        
+# result = list.find_middle()
+# print(result)        
         
 
 
+def iter():
+    my_list = [1, 2, 3, 4, 5]
+    global name
+    name = "Dheeraj"
+    for i in my_list:
+        print(i)
+    print(name)
+        
+print(iter(), end= "/n")
+print(name)
